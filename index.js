6067ad3614ee15a0e6703a770bdfd6d79b54d815
mumira-bit <mumiraesther41@gmail.com>
2025-06-04 15:40:51 +0300

    js strings

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Mumira Esther";
+
+const welcomeMessage = "Welcome to Flatbook, Mumira Esther!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, MUMIRA ESTHER!";
+
+const shortGreeting ="Welcome, M!";
